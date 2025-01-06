cf0a14f7c6f681aca9689b96af3bc1fb32291c8f
Chamal1120 <kenway213beep@gmail.com>
2025-01-06 17:38:58 +0530

    finish Colors and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,20 @@ void main () => runApp(MaterialApp(
     appBar: AppBar(
       title: Text('My First App'),
       centerTitle: true,
+      backgroundColor: Colors.amber[600],
     ),
     body: Center(
-      child: Text('Hello Fellow Flocks!'),
+      child: Text(
+        'Hello Fellow Flocks!',
+        style: TextStyle(
+          color: Colors.amber[600],
+          fontFamily: 'SourceSans3',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: null,
+      backgroundColor: Colors.amber[600],
       child: Text('click'),
     ),
   ),
