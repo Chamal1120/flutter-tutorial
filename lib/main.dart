56f205e1b4000922e0643a266bc7ac21c4d9d301
Chamal1120 <kenway213beep@gmail.com>
2025-01-06 17:35:32 +0530

    finish Stateless-Widgets and Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 
 void main () => runApp(MaterialApp(
-  home: Scaffold(
+  home: Home()));
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     appBar: AppBar(
       title: Text('My First App'),
       centerTitle: true,
+      backgroundColor: Colors.amber[600],
     ),
     body: Center(
-      child: Text('Hello Fellow Flocks!'),
+      child: Text(
+        'Hello Fellow Flocks!',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.grey[600],
+          fontFamily: 'SourceSans3',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      onPressed: null,
+      onPressed: () {},
+      backgroundColor: Colors.amber[600],
       child: Text('click'),
     ),
-  ),
-));
+  );
+  }
+}
