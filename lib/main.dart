e59b72a7598c459447e281ecacd19c7cb37906bc
Chamal1120 <kenway213beep@gmail.com>
2025-01-06 19:01:50 +0530

    finish Buttons and Icons

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,44 @@ class Home extends StatelessWidget {
       backgroundColor: Colors.amber[600],
     ),
     body: Center(
-        child: Image.asset('assets/images/image-2.jpg'),
+// Comment all other childs in the Center widget before uncommenting a child
+
+//        child: IconButton(
+//          onPressed: () {
+//            print('You clicked Me!'); // This will be visible in the console
+//          },
+//          icon: Icon(
+//            Icons.alternate_email,
+//            color: Colors.black,
+//          )
+//        ),
+//        child: ElevatedButton.icon(
+//          onPressed: () {}, 
+//          style: ButtonStyle(
+//            backgroundColor: WidgetStatePropertyAll(Colors.amber[600]),
+//            foregroundColor: WidgetStatePropertyAll(Colors.black),
+//          ),
+//          icon: Icon(
+//            Icons.mail,
+//            color: Colors.black,
+//          ),
+//          label: Text('Mail Me :)'),
+//        ),
+//        child: ElevatedButton(
+//          onPressed: () {
+//            print('You clicked Me!'); // This will be visible in the console
+//          },
+//          style: ButtonStyle(
+//            backgroundColor: WidgetStatePropertyAll(Colors.amber[600]),
+//            foregroundColor: WidgetStatePropertyAll(Colors.black),
+//          ),
+//          child: Text('This is an ElevatedButton'),
+//        ),
+//        child: Icon(
+//          Icons.airport_shuttle,
+//          color: Colors.amber[600],
+//          size: 50.0,
+//        )
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
