134ca952d862538733873833ac02f393dd7899e6
Chamal1120 <kenway213beep@gmail.com>
2025-01-07 01:19:49 +0530

    finish Ninja ID Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,102 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(MaterialApp(
-  home: Home()));
+void main() => runApp(MaterialApp(home: NinjaCard()));
 
-class Home extends StatelessWidget {
-  const Home({super.key});
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-      backgroundColor: Colors.amber[600],
-    ),
-    body: Row(
-        children: [
-          Expanded(
-            flex: 2,
-            child: Image.asset('assets/images/image-2.jpg')
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text(
+          'Ninja ID Card',
+          style: TextStyle(
+            color: Colors.white,
           ),
-          //Expanded(
-          //  flex: 3,
-          //  child: Container(
-          //    padding: EdgeInsets.all(40.0),
-          //    color: Colors.blue,
-          //    child: Text('1'),
-          //  ),
-          //),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(40.0),
-              color: Colors.green,
-              child: Text('2'),
-            ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(40.0),
-              color: Colors.red,
-              child: Text('3'),
-            ),
-          ),
-        ],
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
       ),
-    floatingActionButton: FloatingActionButton(
-      onPressed: () {},
-      backgroundColor: Colors.amber[600],
-      child: Text(':-)'),
-    ),
-  );
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/hotarou.jpg'),
+                radius: 60.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME:',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              'Chamal Randika',
+              style: TextStyle(
+                color: Colors.amber[200],
+                letterSpacing: 2,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Text(
+              'CURRENT NINJA LEVEL:',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text(
+              '9',
+              style: TextStyle(
+                color: Colors.amber[200],
+                letterSpacing: 2,
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              children: [
+                Icon(Icons.email,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10.0,),
+                Text(
+                  'chamal@cooldomain.ninja',
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1.0,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
