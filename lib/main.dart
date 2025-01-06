53f58744ab944eb6476df539e3743fac93adb157
Chamal1120 <kenway213beep@gmail.com>
2025-01-06 20:05:03 +0530

    finish Rows

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,29 @@ class Home extends StatelessWidget {
       centerTitle: true,
       backgroundColor: Colors.amber[600],
     ),
-    body: Padding(
-      // Wrapping inside a padding widget will help to add padding to any widget
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-          // padding adds spacing inside and margin adds for outside
-          padding: EdgeInsets.all(20.0), // Adds padding to all 4 corners
-          //padding: EdgeInsets.fromLTRB(10, 15, 20, 25), // Adds padding sperately to left, top, right, bottom
-          //padding: EdgeInsets.symmetric(horizontal: 10, vertical: 15), // Adds pddings sperately to horizontal and vertical sides
-          margin: EdgeInsets.all(10.0),
-          color: Colors.amber[200],
-          child: Text('This Text is inside a  Container'),
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text('Text Widget'),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+              padding: EdgeInsets.all(20.0),
+              margin: EdgeInsets.all(10.0),
+              color: Colors.amber[200],
+              child: Text('Container'),
+            ),
         ),
+        TextButton(
+            onPressed: () {}, 
+            style: ButtonStyle(
+              backgroundColor: WidgetStatePropertyAll(Colors.amber),
+              foregroundColor: WidgetStatePropertyAll(Colors.black),
+            ),
+            child: Text('TextButton'), 
+          ),
+      ],
     ),
     floatingActionButton: FloatingActionButton(
       onPressed: () {},
